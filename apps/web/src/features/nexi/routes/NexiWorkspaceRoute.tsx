e891b13077c7484328f982bef7e17ca5eaffb374
Chris <chris1bata@gmail.com>
2026-07-27 09:35:08 -0400

    checkpoint(architecture): make shared router authoritative

--- apps/web/src/features/nexi/routes/NexiWorkspaceRoute.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { NexiChatFeature } from "../components/NexiChatFeature";
-
-export function NexiWorkspaceRoute(props: { tenantId: string }): React.ReactElement {
-  return <NexiChatFeature tenantId={props.tenantId} />;
-}
--- apps/web/src/features/scheduling/routes/SchedulingWorkspaceRoute.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { SchedulePanel } from "../components/SchedulePanel";
-
-export function SchedulingWorkspaceRoute(props: { tenantId: string }): React.ReactElement {
-  return <SchedulePanel tenantId={props.tenantId} />;
-}
